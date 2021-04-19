905c138a8166863f74614923ea76bf5dbe492328
oakhtar147 <osamaakhtar15@gmail.com>
2021-04-20 01:03:39 +0500

    guards, top/bottom types

--- challenges/dict/src/index.ts
+++ challenges/dict/src/index.ts
@@ -1,7 +1,15 @@
-export type Dict = {};
+export type Dict<T> = {
+  [k: string]: T | undefined;
+};
 
 // Array.prototype.map, but for Dict
-export function mapDict() {}
+export function mapDict<T>(dict: Dict<T>) {
+  const mappedDict: Dict<string> = {};
 
+  Object.keys(dict).map((fileType) => {
+    mappedDict[fileType] = `*.${dict[fileType]}`;
+  });
+}
+//
 // Array.prototype.reduce, but for Dict
 export function reduceDict() {}
--- notes/5-generics-basics.ts
+++ notes/5-generics-basics.ts
@@ -6,19 +6,27 @@ import { HasEmail } from "./1-basics";
  */
 
 // // param determines the value of x
-// function wrappedValue(x: any) {
-//   return {
-//     value: x
-//   };
-// }
+function wrappedValue(x: any) {
+  return {
+    value: x,
+  };
+}
 
 // // type param determines the type of x
-// interface WrappedValue<X> {
-//   value: X;
-// }
+interface WrappedValue<X> {
+  value: X;
+}
 
-// let val: WrappedValue<string[]> = { value: [] };
-// val.value;
+const stringValue: WrappedValue<string> = {
+  value: "isAString",
+};
+
+const shouldNotBeAccepted: WrappedValue<number[]> = {
+  value: "notANumbersArray",
+};
+
+let val: WrappedValue<string[]> = { value: [""] };
+val.value;
 
 /**
  * we can name these params whatever we want, but a common convention
@@ -30,70 +38,79 @@ import { HasEmail } from "./1-basics";
  * -   just like function parameters can have default values
  */
 
-// // for Array.prototype.filter
-// interface FilterFunction<T = any> {
-//   (val: T): boolean;
-// }
+// for Array.prototype.filter
 
-// const stringFilter: FilterFunction<string> = val => typeof val === "string";
-// stringFilter(0); // 🚨 ERROR
-// stringFilter("abc"); // ✅ OK
+// think of generics as creating a generic interface of an object/function where you may
+// not know the type for one of the properties/arguments
+interface FilterFunction<T = any> {
+  (val: T): boolean;
+}
 
-// // can be used with any value
-// const truthyFilter: FilterFunction = val => val;
-// truthyFilter(0); // false
-// truthyFilter(1); // true
-// truthyFilter(""); // false
-// truthyFilter(["abc"]); // true
+const numFil: FilterFunction<number[]> = (val) => typeof val === "number";
+
+const nums = [1, 2, 3, 4, 5, 6];
+numFil(nums);
+
+const stringFilter: FilterFunction<string> = (val) => typeof val === "string";
+stringFilter(0); // 🚨 ERROR
+stringFilter("abc"); // ✅ OK
+
+// can be used with any value
+const truthyFilter: FilterFunction = (val) => val;
+truthyFilter(0); // false
+truthyFilter(1); // true
+truthyFilter(""); // false
+truthyFilter(["abc"]); // true
 
 /**
  * (3) You don't have to use exactly your type parameter as an arg
  * -   things that are based on your type parameter are fine too
  */
 
-// function resolveOrTimeout<T>(promise: Promise<T>, timeout: number): Promise<T> {
-//   return new Promise<T>((resolve, reject) => {
-//     // start the timeout, reject when it triggers
-//     const task = setTimeout(() => reject("time up!"), timeout);
+function resolveOrTimeout<T>(promise: Promise<T>, timeout: number): Promise<T> {
+  return new Promise<T>((resolve, reject) => {
+    // start the timeout, reject when it triggers
+    const task = setTimeout(() => reject("time up!"), timeout);
 
-//     promise.then(val => {
-//       // cancel the timeout
-//       clearTimeout(task);
+    promise.then((val) => {
+      // cancel the timeout
+      clearTimeout(task);
 
-//       // resolve with the value
-//       resolve(val);
-//     });
-//   });
-// }
-// resolveOrTimeout(fetch(""), 3000);
+      // resolve with the value
+      resolve(val);
+    });
+  });
+}
+resolveOrTimeout(fetch(""), 3000);
 
 /**
  * (4) Type parameters can have constraints
  */
 
-// function arrayToDict<T extends { id: string }>(array: T[]): { [k: string]: T } {
-//   const out: { [k: string]: T } = {};
-//   array.forEach(val => {
-//     out[val.id] = val;
-//   });
-//   return out;
-// }
+function arrayToDict<T extends { id: string }>(array: T[]): { [k: string]: T } {
+  const out: { [k: string]: T } = {};
+  array.forEach((val) => {
+    out[val.id] = val;
+  });
+  return out;
+}
 
-// const myDict = arrayToDict([
-//   { id: "a", value: "first", lisa: "Huang" },
-//   { id: "b", value: "second" }
-// ]);
+const myDict = arrayToDict([
+  { id: "a", value: "first", lisa: "Huang" },
+  { id: "b", value: "second" },
+]);
+// myDict.foo.
 
 /**
  * (5) Type parameters are associated with scopes, just like function arguments
  */
 
-// function startTuple<T>(a: T) {
-//   return function finishTuple<U>(b: U) {
-//     return [a, b] as [T, U];
-//   };
-// }
-// const myTuple = startTuple(["first"])(42);
+function startTuple<T>(a: T) {
+  return function finishTuple<U>(b: U) {
+    return [a, b] as [T, U];
+  };
+}
+const myTuple = startTuple(["first"])(42);
 
 /**
  * (6) When to use generics
